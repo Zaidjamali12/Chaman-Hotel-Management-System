77aa9c6087b5f51b5dadca498d06196224fda332
Tumhara Naam <tumharaemail@example.com>
2025-07-23 21:57:55 +0500

    Added data analysis prediction file

--- newTest.py
+++ newTest.py
@@ -1,12 +1,3 @@
-# Hello World print karne ke liye
-print("Hello, World!")
-
-# User se naam puchne ke liye
-naam = input("Aapka naam kya hai? ")
-print("Hello, " + naam + "!")
-
-# Do numbers ka jod nikalne ke liye
-num1 = 5
-num2 = 7
-jod = num1 + num2
-print("Jod hai: ", jod)
+import seaborn as sns
+tips = sns.load_dataset("tips")
+sns.relplot(data=tips, x="total_bill", y="tip", hue="smoker")
